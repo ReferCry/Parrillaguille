08350d6a4654093aa6f3e457eae009990520b449
ReferCry <fernanpolo2003@gmail.com>
2026-06-10 23:36:19 -0500

    feat: add Avalonia project base for Linux

---
+++ Capa_Presentacion_Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Capa_Presentacion_Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
